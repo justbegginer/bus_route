02fad97313072efa5e67ef49f3d5b6ce7c5fcaaf
justbegginer <blackcloudsinthesky@protonmail.com>
2020-11-20 18:13:39 +0300

    realization of Time class

---
+++ Time.cpp
@@ -0,0 +1,75 @@
+
+//
+// Created by raspberry on 20.11.2020.
+//
+
+#include "Time.h"
+
+
+Time::Time() {
+    _minutes = 0;
+    _hours = 9;
+}
+
+Time::Time(int hours, int minutes) {
+    _hours = hours;
+    _minutes = minutes;
+    if (!is_correct_time()){
+        std::cout <<"time ish't correct\n"
+                    "auto time generation :\n"
+                    "hours = 9\n"
+                    "minutes = 0\n";
+        _hours = hours;
+        _minutes = minutes;
+    }
+}
+
+Time::Time(const Time &time) {
+    _hours = time.getHours();
+    _minutes = time.getMinutes();
+}
+
+int Time::getMinutes() const {
+    return _minutes;
+}
+
+int Time::getHours() const {
+    return _hours;
+}
+
+void Time::setMinutes(int minutes) {
+    _minutes = minutes;
+}
+
+void Time::setHours(int hours) {
+    _hours = hours;
+}
+
+bool Time::is_correct_time() const {
+    return (_hours >= 0 && _hours <= 24) && (_minutes>=0 && _minutes < 60);
+}
+
+bool Time::operator<(const Time &rhs) const {
+    return (_hours < rhs._hours || (_hours == rhs._hours && _minutes < rhs._minutes));
+}
+
+bool Time::operator>(const Time &rhs) const {
+    return (_hours > rhs._hours || (_hours == rhs._hours && _minutes > rhs._minutes));
+}
+
+bool Time::operator<=(const Time &rhs) const {
+    return (rhs < *this || rhs == *this);
+}
+
+bool Time::operator>=(const Time &rhs) const {
+    return !(*this > rhs || rhs == *this);
+}
+
+bool Time::operator==(const Time &rhs) const {
+    return _minutes == rhs._minutes &&
+           _hours == rhs._hours;
+}
+
+bool Time::operator!=(const Time &rhs) const {
+    return !(rhs == *this);
+}
