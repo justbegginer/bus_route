c00c0e8c598e9f3a989b564edc6ede99deedb855
justbegginer <blackcloudsinthesky@protonmail.com>
2020-11-26 17:40:19 +0300

    add my own class Vector

---
+++ Vector.cpp
@@ -0,0 +1,5 @@
+//
+// Created by raspberry on 26.11.2020.
+//
+
+#include "Vector.h"
---
+++ Vector.h
@@ -0,0 +1,14 @@
+//
+// Created by raspberry on 26.11.2020.
+//
+
+#ifndef INDIVIDUAL7_VECTOR_H
+#define INDIVIDUAL7_VECTOR_H
+
+
+class Vector {
+
+};
+
+
+#endif //INDIVIDUAL7_VECTOR_H
