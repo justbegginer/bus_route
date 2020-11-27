d4b7462e93b41031a88782a3cf66e93ee4188823
justbegginer <blackcloudsinthesky@protonmail.com>
2020-11-27 23:52:30 +0300

    add functions and fields to Vector header

--- Vector.h
+++ Vector.h
@@ -1,13 +1,24 @@
 //
 // Created by raspberry on 26.11.2020.
 //
-
+#pragma once
 #ifndef INDIVIDUAL7_VECTOR_H
 #define INDIVIDUAL7_VECTOR_H
-
+#include "BusRoute.h"
 
 class Vector {
-
+private:
+    BusRoute *routes;
+    int length;
+public:
+    Vector();
+    Vector(BusRoute *routes , int length);
+    Vector(const Vector &vector);
+    void push(BusRoute busRoute);
+    void pop();
+    BusRoute& operator[](int index);
+    int size();
+    ~Vector();
 };
 
 
